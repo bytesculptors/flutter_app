1af6d7fbc6d92861a03cd1a2a2f391371ff92a3d
bytesculptors <tuannghia200603@gmail.com>
2024-03-08 17:05:34 +0700

    Add button

--- lib/pages/employee.dart
+++ lib/pages/employee.dart
@@ -56,7 +56,7 @@ class _EmployeeState extends State<Employee> {
               height: 20.0,
             ),
             Text(
-              "Name",
+              "Age",
               style: TextStyle(
                   color: Colors.black,
                   fontSize: 24.0,
@@ -73,6 +73,40 @@ class _EmployeeState extends State<Employee> {
               child: TextField(
                 decoration: InputDecoration(border: InputBorder.none),
               ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Location",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Container(
+              padding: EdgeInsets.only(left: 10.0),
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  borderRadius: BorderRadius.circular(10)),
+              child: TextField(
+                decoration: InputDecoration(border: InputBorder.none),
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Center(
+              child: ElevatedButton(
+                  onPressed: () => {},
+                  child: Text(
+                    "Add",
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  )),
             )
           ],
         ),
